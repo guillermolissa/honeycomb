7e83bb69e210884075959861a6db6bdc1b541dc2
Guillin <guillermo.lissa@gmail.com>
2020-07-18 23:21:40 -0300

    Fix many things from train_models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -17,50 +17,6 @@ import config
 import model_dispatcher
 import metric_dispatcher
 
-
-plot_roc_curve_cv(model, X, y, cv):
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
-
-    fig, ax = plt.subplots()
-    for i, (train, test) in enumerate(cv.split(X, y)):
-        model.fit(X[train], y[train])
-        viz = plot_roc_curve(model, X[test], y[test],
-                            name='ROC fold {}'.format(i),
-                            alpha=0.3, lw=1, ax=ax)
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
-        
-
-    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-            label='Chance', alpha=.8)
-
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(mean_fpr, mean_tpr, color='b',
-            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-            lw=2, alpha=.8)
-
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. dev.')
-
-    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-        title="Receiver operating characteristic example")
-    ax.legend(loc="lower right")
-    plt.show()
-
-    return aucs
-
-
-
 # load datasets
 def load_data(kind):
     
@@ -209,41 +165,41 @@ def run(kind, model, folds, metric):
 
 
 
-	# read the training data with folds df = pd.read_csv(config.TRAINING_FILE)
-	# training data is where kfold is not equal to provided fold # also, note that we reset the index
-	df_train = df[df.kfold != fold].reset_index(drop=True)
-	# validation data is where kfold is equal to provided fold
-	df_valid = df[df.kfold == fold].reset_index(drop=True) # drop the label column from dataframe and convert it to
-	# a numpy array by using .values.
+    # read the training data with folds df = pd.read_csv(config.TRAINING_FILE)
+    # training data is where kfold is not equal to provided fold # also, note that we reset the index
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    # validation data is where kfold is equal to provided fold
+    df_valid = df[df.kfold == fold].reset_index(drop=True) # drop the label column from dataframe and convert it to
+    # a numpy array by using .values.
 
-	# target is label column in the dataframe
-	x_train = df_train.drop("label", axis=1).values y_train = df_train.label.values
-	# similarly, for validation, we have
-	x_valid = df_valid.drop("label", axis=1).values y_valid = df_valid.label.values
-	# fetch the model from model_dispatcher
-	clf = model_dispatcher.models[model]
-	# fir the model on training data
-	clf.fit(x_train, y_train)
-	# create predictions for validation samples
-	preds = clf.predict(x_valid)
-	# calculate & print accuracy
-	accuracy = metrics.accuracy_score(y_valid, preds) print(f"Fold={fold}, Accuracy={accuracy}")
-	# save the model
-	joblib.dump( clf,
-	os.path.join(config.MODEL_OUTPUT, f"dt_{fold}.bin") )
+    # target is label column in the dataframe
+    x_train = df_train.drop("label", axis=1).values y_train = df_train.label.values
+    # similarly, for validation, we have
+    x_valid = df_valid.drop("label", axis=1).values y_valid = df_valid.label.values
+    # fetch the model from model_dispatcher
+    clf = model_dispatcher.models[model]
+    # fir the model on training data
+    clf.fit(x_train, y_train)
+    # create predictions for validation samples
+    preds = clf.predict(x_valid)
+    # calculate & print accuracy
+    accuracy = metrics.accuracy_score(y_valid, preds) print(f"Fold={fold}, Accuracy={accuracy}")
+    # save the model
+    joblib.dump( clf,
+    os.path.join(config.MODEL_OUTPUT, f"dt_{fold}.bin") )
 	
 
 
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument( "--kind",   type=str )
-	parser.add_argument( "--model",	 type=str )
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "--kind",   type=str )
+    parser.add_argument( "--model",	 type=str )
     parser.add_argument( "--folds",  type=int )
     parser.add_argument( "--metric", type=str )
-	args = parser.parse_args()
-	
+    args = parser.parse_args()
+
     assert args.kind not in ['classification', 'regression'], "'kind' should be 'classification' or 'regression'."
 
-	run( fold=args.kind, model=args.model, args.folds, args.metric)
+    run( fold=args.kind, model=args.model, folds=args.folds, metric=args.metric)
