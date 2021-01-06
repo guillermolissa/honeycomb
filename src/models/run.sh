45d2f321a61c551bd351ab42155e1a01d868e65a
Guillermo Lissa <guillermo.lissa@gmail.com>
2021-01-05 23:57:19 -0300

    run script bash was added into models folders

---
+++ src/models/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "******************* TRAINING MODELS *******************"
+# Put here whatever you wanna try
+# echo
+# python train_cvmodel.py --model decision_tree_gini --metric roc_auc --folds 3 --kind classification
+# echo
+# python train_cvmodel.py --model decision_tree_entropy --metric roc_auc --folds 5 --kind classification
+echo "************************  END   ************************"
