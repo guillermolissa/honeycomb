82f1562964ae4c428ebdebaf309a02f122087e94
Guillermo Lissa <guillermo.lissa@gmail.com>
2021-01-09 21:51:59 -0300

    nothin important

--- src/models/run.sh
+++ src/models/run.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-echo "******************* TRAINING MODELS *******************"
-# Put here whatever you wanna try
+
+# echo "******************* TRAINING CV MODELS *******************"
+
+# FOLDS=3
+
 # echo
-# python train_cvmodel.py --model decision_tree_gini --metric roc_auc --folds 3 --kind classification
+# python train_cvmodel.py --model decision_tree_gini --metric roc_auc --folds $FOLDS --kind classification
 # echo
-# python train_cvmodel.py --model decision_tree_entropy --metric roc_auc --folds 5 --kind classification
-echo "************************  END   ************************"
+# python train_cvmodel.py --model decision_tree_entropy --metric roc_auc --folds $FOLDS --kind classification
+# echo
+# python train_cvmodel.py --model rf --metric roc_auc --folds $FOLDS --kind classification
+# echo
+# python train_cvmodel.py --model lgbm --metric roc_auc --folds $FOLDS --kind classification
+# echo
+# python train_cvmodel.py --model xgb --metric roc_auc --folds $FOLDS --kind classification
+# echo
+# echo "************************  END   ************************"
+
+
+# echo "******************* TRAINING MODELS *******************"
+# echo
+# python train_model.py --model decision_tree_gini --metric roc_auc --kind classification
+# echo
+# python train_model.py --model decision_tree_entropy --metric roc_auc --kind classification
+# echo
+# python train_model.py --model rf --metric roc_auc --kind classification
+# echo
+# python train_model.py --model lgbm --metric roc_auc --kind classification
+# echo
+# python train_model.py --model xgb --metric roc_auc --kind classification
+# echo
+# echo "************************  END   ************************"
