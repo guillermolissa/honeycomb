3c6c4890ce15211a250b3ffdf85b8d9492a39a66
Guillermo Lissa <guillermo.lissa@gmail.com>
2021-03-14 00:08:39 -0300

    base build features script

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -8,63 +8,34 @@ from pathlib import Path
 import argparse
 import pickle
 import config
+from feateng_functions import load_data, save_data
+from feateng_functions import build_numerical_feateng, build_cat_le, build_cat_from_le
 
-# Add here whatever lib you need in order to build features
-from sklearn.preprocessing import LabelEncoder
 
 
-# LOAD DATASET
-def load_data(input_file):
-    data  = pd.read_pickle(f"{config.INPUT_PATH}{input_file}.pkl")
-
-    return data
-
-# SAVE DATASET
-def save_data(output_file, data):
-    
-    with open(config.OUTPUT_PATH + f'{output_file}.pkl','wb') as f:
-        pickle.dump(data, f)
-    
-    return 0
-
-# PROCESS DATA
-def build_features(data):
-    """ You must define here how to build any features engineer from data
+def main(input_file, output_file):
+    """ 
+        You must define here how to build any features engineer from data
         :data: dataset from where features will be made
         :features: final dataset with all features eng
     """
-    
-    # ****************************************************** # 
-    # put here what you think is needed to build features 
-    # from your processed data
-    # Ex: 
-    # features = data.copy()
-    #for cat in config.cat_features:
-    #    le = LabelEncoder()
-    #    le.fit(list(data[cat].unique()))
-    #    features[cat] = le.transform(data[cat])
-    #features = preprocessor.fit_transform(data)
-    #features = pd.DataFrame(data=features)
-    # ****************************************************** #
 
-    return features
-    pass
-
-def main(input_file, output_file):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
     logger = logging.getLogger(__name__)
-    logger.info('INIT: making features data set from processed data')
+    logger.info('INIT: build features engineer from processed data')
 
     logger.info('RUN: loading data')
-    df = load_data(input_file)
+    df = load_data(input_file, kind='csv')
    
     logger.info(f'RUN: data size before be processed: {df.shape}')
     
     logger.info(f'RUN: building features')
 
-    features = build_features(df)
+   
+    # ****************************************************** # 
+    # put here what you think is needed to build features 
+    # ****************************************************** #
+    
+    features = None
     del df 
 
     logger.info(f'RUN: features size : {features.shape}')
