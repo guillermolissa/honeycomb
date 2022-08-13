a1353ed7cb459c49e890389ae2690a3442ee18f5
Guillin <guillermo.lissa@gmail.com>
2022-08-12 23:04:22 -0300

    add main config file

---
+++ src/config.py
@@ -0,0 +1,22 @@
+# config.py
+
+class Config:
+    SEED = 42
+    SEC_PER_MIN = 60
+    EPS = 1e-15
+    INPUT_PATH = "data/"
+    MODEL_OUTPUT = "models/"
+    SAVE_MODEL = True
+    TEST_PATH = "data/features/"
+    SUBMIT_PATH = "data/submission/"
+    KAGGLE_PROJECT = "interbank20"
+    KAGGLE_SUBMIT = True
+    NJOBS = -1
+    USEGPU = False
+    VERBOSE = 150
+    PLOT = False
+    SEED = 47
+    ESR = 100            # early_stopping_rounds 
+    LABEL = "target"
+    FOLDS = 5
+    KEY_ID = "customer_ID"
